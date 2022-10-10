8d4d41265b7b60d6762c5842060d3036e5031c31
Marcos Marinho <marcosmlms@outlook.com>
2022-10-10 11:08:06 -0300

    novo

--- js/portifolio.js
+++ js/portifolio.js
@@ -10,3 +10,17 @@ function typeWrite(elemento){
 }
 const titulo = document.querySelector('.titulo-principal');
 typeWrite(titulo);
+
+function mostrar(){
+    var mostrar = [document.getElementById('p4'), document.getElementById('p5'),document.getElementById('p6')]
+ 
+    if (mostrar[0].style.display== "none" || mostrar[1].style.display == "none", mostrar[2].style.display == "none") {
+      mostrar[0].style.display == "block",
+      mostrar[1].style.display == "block",
+      mostrar[2].style.display == "block"
+    } else {
+      mostrar[0].style.display == "none",
+      mostrar[1].style.display == "none",
+      mostrar[2].style.display == "none"
+    }
+}
