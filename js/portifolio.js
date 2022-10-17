f499652d6f2c9eb3da854514ac62325e193edcfa
MarcosLMarinho <marcosmlms@outlook.com>
2022-10-17 18:25:59 -0300

    scroll

--- js/portifolio.js
+++ js/portifolio.js
@@ -33,22 +33,3 @@ function mostrar(){
       document.querySelector('#ver').value = 'Ver mais'
     }
 }
-
-// Scroll suave
-
-const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
-
-function getDistanceFromTheTop(element) {
-  const id = element.getAttribute("href");
-  return document.querySelector(id).offsetTop;
-}
-
-function scrollToSection(event) {
-  event.preventDefault();
-  const distanceFromTheTop = getDistanceFromTheTop(event.target) - 90;
-  smoothScrollTo(0, distanceFromTheTop);
-}
-
-menuLinks.forEach((link) => {
-  link.addEventListener("click", scrollToSection);
-});
